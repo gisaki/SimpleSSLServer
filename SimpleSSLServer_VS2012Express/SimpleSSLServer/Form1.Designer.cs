44c3e5f0c368db03915a973b702322fae0cb3c93
gisaki <gisaki@gmail.com>
2018-05-23 00:38:21 +0900

    Sample implementation

---
+++ SimpleSSLServer_VS2012Express/SimpleSSLServer/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SimpleSSLServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Listen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Listen
+            // 
+            this.button_Listen.Location = new System.Drawing.Point(39, 39);
+            this.button_Listen.Name = "button_Listen";
+            this.button_Listen.Size = new System.Drawing.Size(75, 23);
+            this.button_Listen.TabIndex = 0;
+            this.button_Listen.Text = "Listen";
+            this.button_Listen.UseVisualStyleBackColor = true;
+            this.button_Listen.Click += new System.EventHandler(this.button_Listen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button_Listen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Listen;
+    }
+}
+
---
+++ SimpleSSLServer_VS2012Express/SimpleSSLServer/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleSSLServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static Boolean UserCertificateValidationCallback(Object sender,
+            System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+            System.Security.Cryptography.X509Certificates.X509Chain chain,
+            System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            // always pass
+            return true;
+        }
+
+        private void button_Listen_Click(object sender, EventArgs e)
+        {
+            TcpListener listener = TcpListener.Create(8883);
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            SslStream sslStream = new SslStream(client.GetStream(), false, UserCertificateValidationCallback, null);
+            try
+            {
+                {
+                    System.Security.Cryptography.X509Certificates.X509Certificate2 serverCert =
+                        new System.Security.Cryptography.X509Certificates.X509Certificate2("example_com.pfx", "password");
+                    sslStream.AuthenticateAsServer(serverCert, false, SslProtocols.Tls12, false);
+                    sslStream.ReadTimeout = 2000;
+
+                    byte[] rcvbytes = new byte[128];
+                    int n;
+                    while ((n = sslStream.Read(rcvbytes, 0, rcvbytes.Length)) > 0)
+                    {
+                        String s = BitConverter.ToString(rcvbytes);
+                        Console.WriteLine("Receive: {0}", s);
+                    }
+                }
+            }
+            catch (AuthenticationException ex)
+            {
+                Console.WriteLine("AuthenticationException: {0}", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+            finally
+            {
+                sslStream.Close();
+                client.Close();
+                listener.Stop();
+            } 
+        }
+    }
+}
